08672b8c12277444ffac2bf119fdd43561b7cec3
Simran <simran_palia@yahoo.co.in>
2019-03-18 16:23:12 +0800

    Commit v1.0

--- ExecuteKillFromCommandPrompt/Program.cs
+++ ExecuteKillFromCommandPrompt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace ExecuteKillFromCommandPrompt
     {
         static void Main(string[] args)
         {
+            List<string> processesToKill = new List<string>();
+
+            processesToKill.Add("notepad");
+            processesToKill.Add("notepad++");
+
+            var processes = Process.GetProcesses();
+
+            foreach (var item in processes)
+            {
+                if (processesToKill.Any(x => x == item.ProcessName))
+                {
+                    Console.WriteLine("Killing... " + item.ProcessName);
+                    item.Kill();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
